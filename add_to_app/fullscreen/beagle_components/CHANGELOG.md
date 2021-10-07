ab23970d2d5e1325570a9abf8fdfe3965c9ade21
Hernand Azevedo <hernand.azevedo@zup.com.br>
2021-10-07 16:14:25 -0300

    Flutter native

---
+++ add_to_app/fullscreen/beagle_components/CHANGELOG.md
@@ -0,0 +1,24 @@
+## 0.9.4-alpha
+
+* Adds SimpleForm widget
+* Adds Navigation part 1
+* Adds Analytics
+* Adds ConfirmAction  
+* Updates yoga_engine dependency
+
+## 0.9.3-alpha
+
+* Adds PullToRefresh widget
+* Updates yoga_engine dependency
+
+## 0.9.2-alpha
+
+* Updates Beagle dependency
+
+## 0.9.1-alpha
+
+* Updates Beagle version with support to iOS platform
+
+## 0.9.0-alpha
+
+* Prerelease with the main components
---
+++ add_to_app/fullscreen/beagle_components/lib/beagle_components.dart
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+library beagle_components;
+
+export 'package:beagle_components/src/default_actions.dart';
+export 'package:beagle_components/src/default_components.dart';
+export 'package:beagle_components/src/beagle_button.dart';
+export 'package:beagle_components/src/beagle_container.dart';
+export 'package:beagle_components/src/beagle_image.dart';
+export 'package:beagle_components/src/beagle_lazy_component.dart';
+export 'package:beagle_components/src/beagle_page_indicator.dart';
+export 'package:beagle_components/src/beagle_page_view.dart';
+export 'package:beagle_components/src/beagle_screen.dart';
+export 'package:beagle_components/src/beagle_scroll_view.dart';
+export 'package:beagle_components/src/beagle_tab_bar.dart';
+export 'package:beagle_components/src/beagle_text.dart';
+export 'package:beagle_components/src/beagle_text_input.dart';
+export 'package:beagle_components/src/beagle_touchable.dart';
+export 'package:beagle_components/src/beagle_webview.dart';
+export 'package:beagle_components/src/text_input_type.dart';
+export 'package:beagle_components/src/beagle_pull_to_refresh.dart';
+export 'package:beagle_components/src/after_layout.dart';
+export 'package:beagle_components/src/beagle_simple_form.dart';
+export 'package:beagle_components/src/action/beagle_submit_form.dart';
+export 'package:beagle_components/src/dynamic_list/beagle_dynamic_list.dart';
+export 'package:beagle_components/src/dynamic_list/beagle_dynamic_list_direction.dart';
+export 'package:beagle_components/src/utils/beagle_widget_state_provider.dart';
